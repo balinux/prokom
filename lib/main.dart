c608a0dc0ff39908a5b44a611e6e92ad63eee30b
balinux <ryojuniyantara@gmail.com>
2021-09-15 07:30:13 +0800

    lifecycle state

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,42 @@ void main() {
 }
 
 // appbar scaffoeld
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // TODO: implement didChangeAppLifecycleState
+    super.didChangeAppLifecycleState(state);
+
+// check status state app saat inactive
+    if (state == AppLifecycleState.inactive) {
+      print("====== INACTIVE =======");
+    } else if (state == AppLifecycleState.paused) {
+      print("====== PAUSED =======");
+    } else if (state == AppLifecycleState.resumed) {
+      print("====== RESUMED =======");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
