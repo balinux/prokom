8e56fbbb1834ad12c6fa96e4f2dbbb33d7921956
balinux <ryojuniyantara@gmail.com>
2021-11-03 09:44:17 +0800

    update: materi ke 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,55 +11,50 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-
-// check status state app saat inactive
-    if (state == AppLifecycleState.inactive) {
-      print("====== INACTIVE =======");
-    } else if (state == AppLifecycleState.paused) {
-      print("====== PAUSED =======");
-    } else if (state == AppLifecycleState.resumed) {
-      print("====== RESUMED =======");
-    }
-  }
+class _MyAppState extends State<MyApp> {
+  String name = 'putu';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Pemrograman Komputer dan Mobile",
-          style: TextStyle(
-              color: Colors.blue, fontSize: 24, fontStyle: FontStyle.italic),
-        ),
+        title: Text('Belajar Navigasi'),
       ),
       body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(10.0),
-          color: Colors.amber[600],
-          width: 48.0,
-          height: 48.0,
-          child: Center(
-            child: Image.asset('assets/images/image1.png')
-          ),
+        child: ElevatedButton(
+          onPressed: () {
+            Route route = MaterialPageRoute(builder: (context) => ProfilePage());
+            Navigator.push(context, route);
+          },
+          child: Text($name),
         ),
       ),
-    ));
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tentang Aplikasi'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Kembali ke Beranda'),
+        ),
+      ),
+    );
   }
 }
